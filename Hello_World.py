4dbf970328575c415f22b5c3b8f487302ff9e663
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2019-09-12 18:49:15 +0000

    changes i made

---
+++ Hello_World.py
@@ -0,0 +1,13 @@
+#!user/bin/env python3
+
+#Created by: RJ Fromm
+#Created on: September 2019
+#This is a Hello World program
+
+
+def main():
+    print("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
